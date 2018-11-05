e4778d6d805e73b34355c72fd4433611cf644a15
gaolj <36577543@qq.com>
2018-11-05 18:41:46 +0800

    添加项目文件。

---
+++ ctrltest/stdafx.h
@@ -0,0 +1,18 @@
+// This is a part of the Microsoft Foundation Classes C++ library.
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// This source code is only intended as a supplement to the
+// Microsoft Foundation Classes Reference and related
+// electronic documentation provided with the library.
+// See these sources for detailed information regarding the
+// Microsoft Foundation Classes product.
+#define _WIN32_WINNT 0x0501
+#define NO_WARN_MBCS_MFC_DEPRECATION
+
+#define VC_EXTRALEAN
+
+#include <afxwin.h>
+#include <afxext.h> // for bitmap button
+#include <afxcmn.h> // for spin control
+#include <atlbase.h>
+#include <atlwin.h>
---
+++ modeless/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+// This is a part of the Microsoft Foundation Classes C++ library.
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// This source code is only intended as a supplement to the
+// Microsoft Foundation Classes Reference and related
+// electronic documentation provided with the library.
+// See these sources for detailed information regarding the
+// Microsoft Foundation Classes product.
+#define _WIN32_WINNT 0x0501
+#define NO_WARN_MBCS_MFC_DEPRECATION
+
+#define VC_EXTRALEAN
+
+#include <afxwin.h>      // MFC core and standard components
+#include <afxext.h>         // MFC extensions
